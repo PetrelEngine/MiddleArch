c3770d0ab42d669b343b67a24ddd7b51c71f96a5
liuqian8 <liuqian5356@qq.com>
2019-07-03 16:00:00 +0800

    纹理格式进行判断设定格式。

--- Source/Graph/Texture.cpp
+++ Source/Graph/Texture.cpp
@@ -122,7 +122,20 @@ bool Texture::getParametersDirty() const
 
 unsigned Texture::getExternalFormat(unsigned format)
 {
-    return format;
+    if (format == GL_DEPTH_COMPONENT16 || format == GL_DEPTH_COMPONENT24 || format == GL_DEPTH_COMPONENT32F)
+        return GL_DEPTH_COMPONENT;
+    else if (format == GL_DEPTH24_STENCIL8 || format == GL_DEPTH32F_STENCIL8)
+        return GL_DEPTH_STENCIL;
+    else if (format == GL_R8 || format == GL_R16F || format == GL_R32F)
+        return GL_RED;
+    else if (format == GL_RG8 || format == GL_RG16F || format == GL_RG32F)
+        return GL_RG;
+    else if (format == GL_RGBA16F || format == GL_SRGB_ALPHA_EXT)
+        return GL_RGBA;
+    else if (format == GL_SRGB_EXT)
+        return GL_RGB;
+    else
+        return format;
 }
 
 unsigned Texture::getDataType(unsigned format)
